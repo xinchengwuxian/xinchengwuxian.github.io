66560584b7b8e4da5363e358f6392a10370e36ae
hwpeng <2670949833@qq.com>
2017-09-28 19:42:50 -0700

    Site updated: 2017-09-28 19:42:48

--- js/script.js
+++ js/script.js
@@ -1,137 +1,75 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+(function($) {
+  // scroll-spy
+  if ($('#navbar-toc').get(0)) {
+    $('body').scrollspy({ target: '#navbar-toc' });
+    $('#toc').css('min-width', $('#navbar-toc').css('width'));
+  }
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
-
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
-
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
-    $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
-  });
-
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
-  });
-
-  // Share
-  $('body').on('click', function(){
-    $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
-    e.stopPropagation();
-
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
-
-    if ($('#' + id).length){
-      var box = $('#' + id);
-
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
-      var html = [
-        '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
-        '</div>'
-      ].join('');
-
-      var box = $(html);
-
-      $('body').append(box);
-    }
-
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
-    }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
-  });
-
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
+  // Article
+  $('.article').each(function(i) {
+    // image
+    $(this).find('img').each(function() {
+      if ($(this).parent().hasClass('fancybox-button')) return;
+      if ($(this).parent().get(0).nodeName.toLowerCase() === 'a') return;
       var alt = this.alt;
+      if (alt) $(this).after('<span class="funcybox-caption">' + alt + '</span>');
 
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox-button"></a>');
     });
 
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
+    $(this).find('.fancybox-button').each(function(j) {
+      $(this).attr('rel', 'fancybox-button');
+    });
+
+    // table
+    $(this).find('table').each(function() {
+      if (!$(this).hasClass('table-bordered')) {
+        $(this).addClass('table');
+        $(this).addClass('table-bordered');
+      }
     });
   });
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
+  if ($.fancybox) {
+    $('.fancybox-button').fancybox({
+      helpers : {
+        title : {
+          type : 'over'
+        },
+        buttons : {},
+        overlay : {
+          css : {
+            'background' : 'rgba(58, 42, 45, 0.5)'
+          }
+        }
+      },
+      afterShow : function() {
+        /* Add watermark */
+        $('<div class="watermark"></div>').bind("contextmenu", function(e) {
+          return false; /* Disables right click */
+        }).prependTo($.fancybox.inner);
+
+        $(".fancybox-title").wrapInner('<div />').show();
+
+        $(".fancybox-wrap").hover(function() {
+          $(".fancybox-title").show();
+        }, function() {
+          $(".fancybox-title").hide();
+        });
+      }
+    });
   }
 
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
-
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
+  $.fn.chk_userlanguage = function() {
+    /* check if <style=display:none;> not set to that element */
+    // if (!this.is(":hidden")) { this.hide(); };
+    /* get browser default lang */
+    if (navigator.userLanguage) {
+      baseLang = navigator.userLanguage.substring(0, 2).toLowerCase();
+    }
+    else {
+      baseLang = navigator.language.substring(0, 2).toLowerCase();
+    }
+    console.log(baseLang);//zh
   };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
-    $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
-  });
-
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
-
-    $container.removeClass('mobile-nav-on');
-  });
 })(jQuery);
